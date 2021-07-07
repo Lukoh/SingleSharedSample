077fe785186e02af92f857cf2d4b9e3d4da820bc
Lukoh-Nam <kulho.nam@bluewhale.foundation>
2021-07-07 09:08:46 +0900

    Code Refactoring

--- app/src/main/java/com/goforer/base/extension/IsNull.kt
+++ app/src/main/java/com/goforer/base/extension/IsNull.kt
@@ -18,16 +18,14 @@ package com.goforer.base.extension
 
 inline fun <reified T> T?.isNull(
     crossinline doOnIsNull: () -> Unit,
-    crossinline doOnIsNotNull: (T) -> Unit
-) {
+    crossinline doOnIsNotNull: (T) -> Unit) {
     this ?: doOnIsNull.invoke()
     this?.let(doOnIsNotNull)
 }
 
 suspend inline fun <reified T> T?.isNullOnFlow(
     crossinline doOnIsNull: suspend () -> Unit,
-    crossinline doOnIsNotNull: suspend (T) -> Unit
-) {
+    crossinline doOnIsNotNull: suspend (T) -> Unit) {
     this ?: doOnIsNull.invoke()
     this?.let {
         doOnIsNotNull.invoke(it)
